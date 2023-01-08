97039dd84d415b643c275bbde607b9477a784591
l bates childs <labchilds@gmail.com>
2023-01-07 21:35:58 -0500

    add Place result models

---
+++ FidoFriendly/Models/DogFriendlyPlace.swift
@@ -0,0 +1,48 @@
+//
+//  DogFriendlyPlace.swift
+//  FidoFriendly
+//
+//  Created by Lelah Bates Childs on 1/7/23.
+//
+
+import Foundation
+
+struct DogFriendlyPlace: Codable {
+    var fsqId: String
+    var placeName: String
+    // let geocode: Geocode
+    var categories: [Category]
+    var location: Location
+    var distance: Double
+    var link: String
+    var description: String?
+    var tel: String?
+    var website: String?
+    var rating: Double?
+    
+    enum CodingKeys: String, CodingKey {
+        case fsqId = "fsq_id"
+        case placeName = "name"
+        // case geocode = "geocodes"
+        case categories, location, distance, link, description, tel, website, rating
+    }
+}
+
+struct Location: Codable {
+    let address: String
+    let city: String
+    let crossStreet: String?
+    let region: String?
+    let postcode: String?
+    let country: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case address, region, postcode, country
+        case city = "locality"
+        case crossStreet = "cross_street"
+    }
+}
+
+struct Category: Codable {
+    let name: String
+}
