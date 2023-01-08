67826a227ab7f86ad0ef9ae1c364271375624d0e
l bates childs <labchilds@gmail.com>
2023-01-08 18:32:11 -0500

    add saved places table view

--- FidoFriendly/Controller/SavedPlacesViewController.swift
+++ FidoFriendly/Controller/SavedPlacesViewController.swift
@@ -15,6 +15,29 @@ class SavedPlacesViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        view.addSubview(savedPlaceTable)
+        
+        savedPlaceTable.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        savedPlaceTable.delegate = self
+        savedPlaceTable.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        savedPlaceTable.frame = view.bounds
     }
 
+}
+
+extension SavedPlacesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Saved Place \(indexPath.row)"
+        return cell
+    }
 }
